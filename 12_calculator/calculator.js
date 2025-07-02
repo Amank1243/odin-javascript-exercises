373fd18a6a5c4f2d9f0a5a2c420b6ec18bb18894
Aman Khajuria <24khajuriaa@gmail.com>
2025-07-01 20:48:37 -0700

    wrk at library

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
+  return arr.reduce((prev, num) => prev + num);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev, next) => prev * next);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) {
+    return 1;
+  }
+  let nums = new Array;
+	for (let i = 1; i <= parseInt(a); i++) {
+    nums.push(i);
+  }
+
+  return nums.reduce((prev, next) => prev * next);
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (word) {
+    let string = new String(word)
+    string = string.toLowerCase();
+    string = string.replace(/[^a-z1-9]/g, '');
+    
+    let arr = string.split('');
+    if (arr.join('') == arr.reverse().join('')) {
+        return true;
+    } else {
+    return false;
+    }
+}
 // Do not edit below this line
 module.exports = palindromes;
